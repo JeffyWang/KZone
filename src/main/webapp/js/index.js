aa4d5f445e481c57a0f3c89a83338c1368c4c762
jeffy <jeffy.chubby@gmail.com>
2014-06-05 22:05:56 +0800

    index

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,27 +1,106 @@
-if ( $.fn.makisu.enabled ) {
-    var $maki = $( '.maki' );
+//if ( $.fn.makisu.enabled ) {
+//    var $maki = $( '.maki' );
+//
+//    $maki.makisu({
+//        selector: 'dd',
+//        overlap: 0.6,
+//        speed: 0.85
+//    });
+//
+//    $( '.list' ).makisu( 'open' );
+//
+//    // Toggle on click
+//
+//    $( '.toggle' ).on( 'click', function() {
+//        $( '.list' ).makisu( 'toggle' );
+//    });
+//
+//    // Disable all links
+//
+//    $( '.demo a' ).click( function( event ) {
+//        event.preventDefault();
+//    });
+//
+//} else {
+//
+//    $( '.warning' ).show();
+//}
 
-    $maki.makisu({
-        selector: 'dd',
-        overlap: 0.6,
-        speed: 0.85
-    });
+var _dataOffset = 0;
+var _pageDataCount = 15;
+var _pageCount = 0;
+var _totalDataCount = 0;
+var _localhostPath = "";
+var _scroll = true;
 
-    $( '.list' ).makisu( 'open' );
+$(document).ready(function(){
+    var curWwwPath = window.document.location.href;
+    var pathName=window.document.location.pathname;
+    var pos=curWwwPath.indexOf(pathName);
+    _localhostPath = curWwwPath.substring(0,pos);
 
-    // Toggle on click
+    getKTVCount();
+})
 
-    $( '.toggle' ).on( 'click', function() {
-        $( '.list' ).makisu( 'toggle' );
-    });
+$(window).scroll( function() {
+    if(_scroll)
+        loadData();
+});
 
-    // Disable all links
+var loadData = function() {
+    var totalHeight = parseFloat($(window).height()) + parseFloat($(window).scrollTop());     //浏览器的高度加上滚动条的高度
+    if ($(document).height() <= totalHeight)     //当文档的高度小于或者等于总的高度的时候，开始动态加载数据
+    {
+        //加载数据
+        console.log("`````````````````");
+//        $("#information").append('<div class="well well-lg">new data</div><div class="well well-lg">...</div><div class="well well-lg">...</div><div class="well well-lg">...</div><div class="well well-lg">asdfasdf</div><div class="well well-lg">...</div><div class="well well-lg">...</div><div class="well well-lg">...</div>');
 
-    $( '.demo a' ).click( function( event ) {
-        event.preventDefault();
-    });
+        _dataOffset = _dataOffset + _pageDataCount;
+        getKTVInfo();
+        if(_dataOffset > _totalDataCount) {
+//            $("#information").append('<div class="well well-lg">已加载完所有内容</div>');
+//            _scroll = false;
+        }
+    }
+}
 
-} else {
+var getKTVInfo = function () {
+    var url = _localhostPath + "/rest/ktv/info?offset=" + _dataOffset + "&length=" + _pageDataCount + "&name=null&address=null&districtId=null";
+    var ktvString = "";
+    $.ajax({
+        url: url,
+        type: "GET",
+        dataType: "json",
+        contentType: 'application/json;charset=utf-8',
+        success: function (data) {
+            console.log(data)
+            $.each(data, function(ktvIndex, ktv) {
+                var imgSrc = JSON.parse(ktv.pictures).bigPictures.split(",")[0];
+                console.log(imgSrc);
+                ktvString += '<div class="col-sm-6 col-md-4"><div class="thumbnail"><img class="ktvImg" src="' + imgSrc + '" alt="..."><div class="caption"><h3>' + ktv.name + '</h3><p>' + ktv.score + '</p><p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p></div></div></div>'
+            })
+            $(".row").append(ktvString);
+        }
+    })
+}
 
-    $( '.warning' ).show();
+var getKTVCount = function() {
+    var url = _localhostPath + "/rest/ktv/count?name=null&address=null&districtId=nll";
+    console.log(1111111111111)
+    $.ajax({
+        url: url,
+        type: "GET",
+        dataType: "json",
+        contentType: 'application/json;charset=utf-8',
+        success: function (data) {
+            _totalDataCount = data.dataCount;
+            if(_totalDataCount % _pageDataCount == 0) {
+                _pageCount = _totalDataCount / _pageDataCount;
+            } else {
+                _pageCount = Math.ceil(_totalDataCount / _pageDataCount);
+            }
+
+            getKTVInfo();
+        }
+    })
 }
